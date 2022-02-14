cac1b4c09c0fa6b79134e95e6a33284ba01fc683
DanBi-Lee <danbi.s.rain@gmail.com>
2022-02-14 23:56:19 +0900

    Add Input component

--- src/components/Meals/MealItemForm.jsx
+++ src/components/Meals/MealItemForm.jsx
@@ -1,16 +1,19 @@
-import { useContext, useState } from "react";
-import { CartDispatchContext } from "../../store/CartContext";
+import {useContext, useState} from "react";
+import {CartDispatchContext} from "../../store/CartContext";
 import Button from "../UI/Button";
+import Input from "../UI/Input";
 import style from "./MealItemForm.module.css";
 
 function MealItemForm({mealData}) {
     const cartDispatch = useContext(CartDispatchContext);
     const {id, title, desc, price} = mealData;
-    const [count, setCount] = useState(0);
+    const [count,
+        setCount] = useState(0);
     const changeCountHandler = (e) => {
         setCount(e.target.value);
     };
-    const addCartHandler = () => {
+    const addCartHandler = (e) => {
+        e.preventDefault();
         const cartItem = {
             id,
             title,
@@ -18,29 +21,25 @@ function MealItemForm({mealData}) {
             price,
             count: count*1
         }
-        if(count<=0){
+        if (count <= 0) {
             alert('1개 이상 입력해야 합니다');
             return;
         }
-        cartDispatch({
-            type: 'ADD_CART',
-            payload: cartItem
-        });
+        cartDispatch({type: 'ADD_CART', payload: cartItem});
     }
     return (
         <form className={style.paymentInfo}>
-            <p>
-                <label htmlFor={`${count}-${id}`}>
-                    Amount
-                </label>
-                <input
-                    type="number"
-                    name={`${count}-${id}`}
-                    id={`${count}-${id}`}
-                    min="0"
-                    value={count}
-                    onChange={changeCountHandler}/>
-            </p>
+            <Input
+                label="Amount"
+                input={{
+                id : `acount-${id}`,
+                min: 0,
+                max: 5,
+                step: 1,
+                defaultValue: count,
+                onChange: changeCountHandler,
+                type: "number"
+            }}/>
             <Button text="+ Add" onClick={addCartHandler}/>
         </form>
     );
