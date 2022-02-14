2671cdc9f401b03c3e454d4ddd64ee74d48933e3
DanBi-Lee <danbi.s.rain@gmail.com>
2022-02-14 22:41:13 +0900

    Add FontAwesome icon to CartButton component

--- src/components/Cart/CartButton.jsx
+++ src/components/Cart/CartButton.jsx
@@ -1,13 +1,18 @@
 import { useContext } from 'react';
 import {CartStateContext} from '../../store/CartContext';
 import style from './CartButton.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 function CartButton({onOpen}){
     const cartState = useContext(CartStateContext);
     console.log(cartState);
     return (
         <button className={style.badge} onClick={onOpen}>
-            <div>Your Cart</div>
+            <div>
+                <FontAwesomeIcon icon={faCartShopping}/>
+                 <span> Your Cart</span>
+            </div>
             <div className={style.count}>{cartState.cartLength}</div>
         </button>
     );
