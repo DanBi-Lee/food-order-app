a8b60abf521659bd11ff38b28b13780adaa56f90
DanBi-Lee <danbi.s.rain@gmail.com>
2022-03-01 12:41:04 +0900

    Added cart button animation

--- src/components/Cart/CartButton.jsx
+++ src/components/Cart/CartButton.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import {CartStateContext} from '../../store/CartContext';
 import style from './CartButton.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,9 +6,26 @@ import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 function CartButton({onOpen}){
     const cartState = useContext(CartStateContext);
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+    let btnClasses = `${style.badge} ${btnIsHighlighted && style.bump}`;
+
+    const {totalAmount} = cartState
+    useEffect(()=>{
+        if(totalAmount === 0){
+            return;
+        }
+        setBtnIsHighlighted(true);
+        const timer = setTimeout(()=>{
+            setBtnIsHighlighted(false);
+        },300);
+        return ()=>{
+            clearTimeout(timer);
+        }
+    },[totalAmount]);
+
     console.log(cartState);
     return (
-        <button className={style.badge} onClick={onOpen}>
+        <button className={btnClasses} onClick={onOpen}>
             <div>
                 <FontAwesomeIcon icon={faCartShopping}/>
                  <span> Your Cart</span>
