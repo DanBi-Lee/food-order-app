b462d03a4ed9bf3fa16a973580ec81c152323bba
DanBi-Lee <danbi.s.rain@gmail.com>
2022-03-06 12:41:50 +0900

    meals데이터 get요청 에러 핸들링 추가

--- src/components/Meals/MealList.jsx
+++ src/components/Meals/MealList.jsx
@@ -1,44 +1,48 @@
-import { useState } from "react";
-import { useEffect } from "react";
-import { getMeals } from "../../http/mealList";
+import {useState} from "react";
+import {useEffect} from "react";
+import {getMeals} from "../../http/mealList";
 import MealItem from "./MealItem";
 import style from "./MealList.module.css"
 
-function MealList(){
-    const [meals, setMeals] = useState({});
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(false);
+function MealList() {
+    const [meals,
+        setMeals] = useState({});
+    const [isLoading,
+        setIsLoading] = useState(false);
+    const [error,
+        setError] = useState(false);
 
-    useEffect( ()=>{
+    useEffect(() => {
         let getData;
         setIsLoading(true);
-        try{
-            getData = async () =>{
+        getData = async() => {
+            try {
                 const data = await getMeals();
                 setMeals(data);
+            } catch (e) {
+                setError(e);
             }
-        }catch(e){
-            setError(e);
         }
         setIsLoading(false);
         getData();
     }, []);
 
-    if(isLoading){
-        return <div>로딩중...</div>
+    if (isLoading) {
+        return <div className={style.loadingBox}>로딩중...</div>
     }
-    if(error) {
-        return <div>에러가 발생했습니다.</div>
+    if (error) {
+        return <div className={style.errorBox}>{error.message}</div>
     }
-    if(Object.keys(meals).length === 0){
-        return <div>항목이 없습니다.</div>
+    if (Object.keys(meals).length === 0) {
+        return <div className={style.emptyBox}>항목이 없습니다.</div>
     }
 
     return (
         <ul className={style.list}>
-            {
-                Object.keys(meals).map(id => <MealItem mealData={meals[id]}  key={meals[id].id} />)
-            }
+            {Object
+                .keys(meals)
+                .map(id => <MealItem mealData={meals[id]} key={meals[id].id}/>)
+}
         </ul>
     )
 }
--- src/http/mealList.js
+++ src/http/mealList.js
@@ -1,7 +1,11 @@
-const url = `${process.env.REACT_APP_FIREBASE_DATABASE_URL}/meals.json`;
+const url = `dskljf/${process.env.REACT_APP_FIREBASE_DATABASE_URL}/meals.json`;
 
 export const getMeals = async () => {
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    throw new Error("응답을 받아오지 못했습니다.");
+  }
 };
